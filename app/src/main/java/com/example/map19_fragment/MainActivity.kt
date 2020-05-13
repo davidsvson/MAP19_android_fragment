7f8cbd3119d00d96c35934e80f1f0ff270e71e91
David Svensson <davidsvson@gmail.com>
2020-05-13 12:02:04 +0200

    FRAGMENTS

--- app/src/main/java/com/example/map19_fragment/MainActivity.kt
+++ app/src/main/java/com/example/map19_fragment/MainActivity.kt
@@ -2,17 +2,75 @@ package com.example.map19_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun addPinkFragment(view : View) {
         val pinkFragment = PinkFragment()
         val transaktion = supportFragmentManager.beginTransaction()
         transaktion.add(R.id.container, pinkFragment, "pinkFragment")
         transaktion.commit()
+    }
+
+    fun removePinkFragment(view : View) {
+        val pinkFragment = supportFragmentManager.findFragmentByTag("pinkFragment")
+        if(pinkFragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.remove(pinkFragment)
+            transaction.commit()
+        } else {
+            Toast.makeText(this, "Pinkfragment not found", Toast.LENGTH_SHORT)
+                .show()
+        }
 
     }
+
+    fun addYellowFragment(view: View) {
+        val yellowFragment = YellowFragment.newInstance("hej", "då")
+        val transaktion = supportFragmentManager.beginTransaction()
+        transaktion.add(R.id.container, yellowFragment, "yellowFragment")
+        transaktion.commit()
+    }
+
+    fun removeYellowFragment(view: View) {
+        val yellowFragment = supportFragmentManager.findFragmentByTag("yellowFragment")
+        if(yellowFragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.remove(yellowFragment)
+            transaction.commit()
+        } else {
+            Toast.makeText(this, "Yellowfragment not found", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    fun replaceWithPinkFragment( view: View) {
+        val fragment = PinkFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment, "pinkFragment")
+        transaction.commit()
+
+    }
+
+    fun changeText(view : View) {
+        val fragment = supportFragmentManager.findFragmentByTag("pinkFragment") as PinkFragment?
+
+        fragment?.setText("Susan")
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/map19_fragment/PinkFragment.kt
+++ app/src/main/java/com/example/map19_fragment/PinkFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class PinkFragment : Fragment() {
+
+    private lateinit var nameText : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,7 +18,14 @@ class PinkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.pink_fragment,container, false)
+        nameText = view.findViewById(R.id.nameText)
+
         return view
+    }
+
+    fun setText(text : String) {
+        nameText.text = text
+        Toast.makeText(requireContext(), "Text changed", Toast.LENGTH_SHORT).show()
     }
 
 }
